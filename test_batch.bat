36d914d43eec432381bf6caddc99610b02845ba2
Ritchie <britchie@agreserves.com>
2022-06-21 12:20:23 -0600

    Shape data changes

--- test_batch.bat
+++ test_batch.bat
@@ -1,12 +1,9 @@
 @echo off
 "C:\Users\britchie\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.10\Python 3.10 (64-bit).lnk" "C:\Users\britchie\OneDrive - AgReserves, Inc\Documents\ShapeFileTest\write_geojson.py"
 
-pause
-
-#!/bin/sh
 cd C:\Users\britchie\OneDrive - AgReserves, Inc\Documents\ShapeFileTest
 git add .
-git commit -am "made changes"
+git commit -am "Shape data changes"
 git push
 echo Press Enter...
 read
